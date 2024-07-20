d6eeff6dec019237b236b68d0b10f5cfc3a2866d
Mateus Ribeiro <mateus.ribeiro@cmtecnologia.com.br>
2024-07-20 01:42:18 -0300

    Criação do primero timestep do codigo

--- src/app/views/landing/landing.component.ts
+++ src/app/views/landing/landing.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 
 @Component({
   selector: "app-landing",
   templateUrl: "./landing.component.html",
+  styleUrls:["./landing.component.css"],
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}
+  }]
 })
 export class LandingComponent implements OnInit {
-  constructor() {}
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
 }
