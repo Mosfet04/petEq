f5ad3a899feb2d491bac1c18bf57c7c470996f7f
Mateus Ribeiro <mateus.ribeiro@cmtecnologia.com.br>
2024-10-18 21:07:30 -0300

    Integração aba de pesquisa

--- src/app/views/atividades/pesquisa/pesquisa.component.ts
+++ src/app/views/atividades/pesquisa/pesquisa.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import axios from "axios";
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: "app-pesquisa",
@@ -8,11 +10,40 @@ export class PesquisaComponent implements OnInit {
   constructor() {}
   openTabAtual = 1;
   openTabAnterior = 1;
+
+  pesquisasColetivasAtiva : any = [];
+  pesquisasColetivasPassada : any = [];
   toggleTabsAtual($tabNumber: number){
     this.openTabAtual = $tabNumber;
   }
   toggleTabsAnterior($tabNumber: number){
     this.openTabAnterior = $tabNumber;
   }
-  ngOnInit(): void {}
+  ngOnInit(): void 
+  {
+    this.fetchPesquisasColetivasAtivas();
+    this.fetchPesquisasColetivasPassadas();
+  }
+
+  async fetchPesquisasColetivasAtivas() {
+    const endpoint = "/pesquisa?ativo=true";
+    try {
+      const response = await axios.get(environment.urlBackEnd + endpoint);
+
+      this.pesquisasColetivasAtiva = response.data.items;
+    } catch (error) {
+      console.error("Erro ao fazer a requisição:", error);
+    }
+  }
+
+  async fetchPesquisasColetivasPassadas() {
+    const endpoint = "/pesquisa?ativo=false";
+    try {
+      const response = await axios.get(environment.urlBackEnd + endpoint);
+
+      this.pesquisasColetivasPassada = response.data.items;
+    } catch (error) {
+      console.error("Erro ao fazer a requisição:", error);
+    }
+  }
 }
