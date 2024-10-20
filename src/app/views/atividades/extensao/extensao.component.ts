2b824da4d433e2e88a2f5d1bde970abfb2e9cf98
Mateus Ribeiro <mateus.ribeiro@cmtecnologia.com.br>
2024-10-19 22:05:04 -0300

    Integraçao com endpoints de controle de extencao

--- src/app/views/atividades/extensao/extensao.component.ts
+++ src/app/views/atividades/extensao/extensao.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import axios from "axios";
+import { list } from "postcss";
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: "app-extensao",
@@ -6,9 +9,31 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ExtensaoComponent implements OnInit {
   constructor() {}
+  listEnsinoAlemDaUfu : any[] = [];
+  listAcoesSociais : any[] = [];
+  listDivulgacao: any[] = [];
+
+  
   openTabEnsino = 1;
   openTabSocial = 1;
   openTabDivulga = 1;
+
+  fetchExtensao() {
+    const endpoint = `${environment.urlBackEnd}/extensao?ativo=true`;
+
+    axios.get(endpoint+"&tipo=1").then((response) => {
+      console.log(endpoint+"&tipo=1", response);
+      this.listEnsinoAlemDaUfu = response.data.items;
+    });
+    axios.get(endpoint+"&tipo=2").then((response) => {
+      this.listAcoesSociais = response.data.items;
+    });
+    axios.get(endpoint+"&tipo=3").then((response) => {
+      this.listDivulgacao = response.data.items;
+    });
+    
+  }
+
   toggleTabsEnsino($tabNumber: number){
     this.openTabEnsino = $tabNumber;
   }
@@ -18,5 +43,8 @@ export class ExtensaoComponent implements OnInit {
   toggleTabsDivulga($tabNumber: number){
     this.openTabDivulga = $tabNumber;
   }
-  ngOnInit(): void {}
+  ngOnInit(): void 
+  {
+    this.fetchExtensao();
+  }
 }
