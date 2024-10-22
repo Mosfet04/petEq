ce306e1bad7638486b00290a6a484094a1001680
Mateus Ribeiro <mateus.ribeiro@cmtecnologia.com.br>
2024-10-22 11:05:00 -0300

    Url de redirecionamento correta

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -4,7 +4,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: "2554d619-2b30-4d13-a032-aa202936d184",
     authority: "https://login.microsoftonline.com/5b7c6db5-8d4d-45c4-afbd-8c66cdff194b",
-    redirectUri: "https://localhost:4200/admin/dashboard",
+    redirectUri: "https://pet-eq.vercel.app/admin/dashboard",
   },
   cache: {
     cacheLocation: "localStorage",
