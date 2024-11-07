169dacb2ff54fb413571b70296eb94376801ad44
Mateus Ribeiro <mateus.ribeiro@cmtecnologia.com.br>
2024-11-07 17:48:30 -0300

    Ajuste apontamento microsoft para o grupo PET

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -3,8 +3,8 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 
 export const msalConfig: Configuration = {
   auth: {
-    clientId: "2554d619-2b30-4d13-a032-aa202936d184",
-    authority: "https://login.microsoftonline.com/5b7c6db5-8d4d-45c4-afbd-8c66cdff194b",
+    clientId: "2f6d798c-85c5-4c07-ab80-04bcabfd0f01",
+    authority: "https://login.microsoftonline.com/3faa0a62-eaff-4053-986f-9ecf9237e044",
     redirectUri: "https://pet-eq.vercel.app/admin/dashboard",
   },
   cache: {
@@ -15,6 +15,5 @@ export const msalConfig: Configuration = {
 
 export function isTokenExpired(token: string): boolean {
   const helper = new JwtHelperService();
-  console.log(helper.isTokenExpired(token), helper.getTokenExpirationDate(token));
   return helper.isTokenExpired(token);
 }
